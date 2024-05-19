5bfbbafa56e7a347acbd7d1a961b6541ed3ceaa5
Uttkarsh <uttkarshsaini10@gmail.com>
2024-05-19 18:48:21 +0530

    SignIn page UI Complete

--- src/Components/Sign/SignIn.js
+++ src/Components/Sign/SignIn.js
@@ -3,18 +3,16 @@ import styles from './SignIn.module.css'
 
 function SignIn() {
   return (
-    <div>
+    <div style={{width:"250px", textAlign:"left", paddingLeft:"650px", paddingTop:"150px"}}>
 
-        <div>Welcome back!</div>
-        <div>Sign in to continue</div>
-        <div>Email</div>
-        <input type="email"/>
+        <div style={{fontSize:"20px", paddingTop:"10px"}}>Welcome back!</div>
+        <div style={{paddingTop : "10px"}}>Sign in to continue</div>
+        <div style={{paddingTop : "20px", paddingBottom:"5px"}}>Email</div>
+        <input type="email" style={{width:"90%", height:"30px", borderRadius:"6px"}}/>
         <div style={{padding:"5px"}}><button className={styles.signIn_continueButton}>Continue</button></div>
+        <div style={{paddingTop : "20px", textAlign:"center"}}>No account?<span style={{padding:"2px", color:"rgb(41, 74, 235)"}}>Create one</span></div>
         
-        <div style={{display:"flex"}}>
-            <div>No account?</div>
-            <div>Create oen</div>
-        </div>
+
 
     </div>
   )
